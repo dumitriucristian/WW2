e3a663f72288139e695eeae4d51f095ab947c963
GrigoAleex <57829500+GrigoAleex@users.noreply.github.com>
2020-05-27 12:45:20 +0100

    Tank methods

--- classes/tank.class.php
+++ classes/tank.class.php
@@ -1,13 +1,16 @@
 <?php
 
 class Tank{
-    public $life;
-    public $attack;
-    public $defence;
-    public $moves;
+    public number $life;
+    public number $attack;
+    public number $defence;
+    public number $moves;
     // TODO: id tank (variabila static in clasa abstracta?)
 
-    function __construct(number $life, number $attack, number $defence, number $moves)
+    protected bool $isDefending = true;
+    protected $armorBonus = 20;
+
+    function __construct($life = 78, $attack = 65, $defence = 175, $moves = 1)
     {
         $this->life = $life;
         $this->attack = $attack;
@@ -18,5 +21,29 @@ class Tank{
     function __destruct()
     {
         echo '<p class="alert"> A tank was destroyed! </p>';
+    }
+
+    public function move()
+    {
+        if($this->isDefending){
+            $this->isDefending = false;
+            $this->defence -= $this->armorBonus;
+        }
+        echo '<p>A tank has moves</p>';
+    }
+
+    public function attack($target){
+        $target->life -= ($this->attack/$target->armor) * 100;
+        if($targte->life <= 0){
+            $target_>__destruct();
+            unset($target);
+        }
+        echo '<p>A tank is destorying the enemy!</p>';
+    }
+
+    public function defence(){
+        $this->isDefending = true;
+        $this->defence += $this->armorBonus;
+        echo '<p>A tank is ready to take a hit</p>';
     }
 }
