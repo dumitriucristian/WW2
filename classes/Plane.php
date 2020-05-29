c652a669129a17fc09026acdda915eca9b81b750
cdumitriu <dumitriucristian@yahoo.com>
2020-05-29 17:41:38 +0300

    draft attack from peer

--- classes/Plane.php
+++ classes/Plane.php
@@ -3,10 +3,31 @@
 
 class Plane extends  Unit
 {
+
     public function __construct()
     {
         $this->life = 50;
         $this->attackPower = 15;
+        $this->attackType = "plane";
+    }
+
+    public function setAttackPower($enemyType)
+    {
+        switch ($enemyType) {
+            case "soldier":
+                $efficiency = 0.1;
+                break;
+            case "tank":
+                $efficiency = 0.5 ;
+                break;
+            case "plane":
+                $efficiency = 1 ;
+                break;
+            default:
+                $efficiency = 1;
+        }
+
+        return $this->attackPower * $efficiency;
     }
 
 }
